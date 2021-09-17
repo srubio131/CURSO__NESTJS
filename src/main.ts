9ed2b5245fd5f964c9a78e1ffb22b4b340740f21
srubio131 <s.rubio131@gmail.com>
2021-09-17 14:10:04 +0200

    Logging

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,16 @@
-import { ValidationPipe } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { TransformInterceptor } from './transform.interceptor';
 
+const serverPort = 3000;
+
 async function bootstrap() {
+  const logger = new Logger();
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalInterceptors(new TransformInterceptor());
-  await app.listen(3000);
+  await app.listen(serverPort);
+  logger.log(`Application listening on port: ${serverPort}`);
 }
 bootstrap();
